b601a16e8f4bd582c757c03e087fa34b2cdbdc8c
Martina Milone <martina_milone@icloud.com>
2025-04-22 23:32:57 +0200

    Iteration 3

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,28 @@ import { ref } from 'vue';
 import contacts from './contacts.json'
 import trophy from './assets/trophy.png'
 
-const topContacts = ref(contacts.slice(0,14))
+const topContacts = ref(contacts.slice(0, 5))
+
+const addBUtton= ref(null);
+
+const addContact = () => {
+  const existingContacts = new Set(topContacts.value.map(contact => contact.id));
+  const remainingContacts = contacts.filter(contact => !existingContacts.has(contact.id));
+  if (remainingContacts.length === 0) {
+    addBUtton.value.innerHTML = 'SORRY, NO MORE CONTACTS LEFT';
+    return
+  };
+  const randomIndex = Math.floor(Math.random()* remainingContacts.length);
+  const randomContact = remainingContacts[randomIndex];
+
+  topContacts.value.unshift(randomContact);
+}
+
 </script>
 
 <template>
   <h1>Iron Contacts</h1>
+  <button ref="addBUtton" @click="addContact">ADD RANDOM CONTACT</button>
   <table>
     <thead>
       <th>Picture</th>
@@ -48,12 +65,29 @@ const topContacts = ref(contacts.slice(0,14))
 
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  width: 100%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
 }
 
 h1 {
   text-align: center;
+}
+
+button {
+  text-align: center;
+  width: 250px;
+  margin: 20px calc((100vw - 250px)/2) 40px;
+  padding: 5px
 }
 
 .portrait {
